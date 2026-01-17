ab74ee56d31ac146debe47c6ed3b8a32f293dbc3
vignesh jayakumar <iamvicky00@gmail.com>
2026-01-18 00:33:38 +0530

    Search in product list and search , filter in Invoice list added

--- src/app/admin/all-products-list/all-products-list.component.ts
+++ src/app/admin/all-products-list/all-products-list.component.ts
@@ -1,21 +1,44 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, switchMap, tap } from 'rxjs';
+import { debounceTime, Subscription, switchMap, tap } from 'rxjs';
 import { NgClass } from '@angular/common';
 import { Router } from '@angular/router';
 
 import { TAdminProductList } from './all-products.modal';
 import { AdminProductService } from '../admin-product.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-all-products-list',
-  imports: [NgClass],
+  imports: [NgClass, ReactiveFormsModule],
   templateUrl: './all-products-list.component.html',
   styleUrl: './all-products-list.component.css',
 })
 export class AllProductsListComponent implements OnInit, OnDestroy {
   subsList: Subscription[] = [];
   productsList: TAdminProductList['response']['products'] = [];
-
+  searchProductList: TAdminProductList['response']['products'] = [];
+  searchInput = new FormControl('');
+  searchObs = this.searchInput.valueChanges
+    .pipe(
+      debounceTime(500),
+      tap((searchTerm) => {
+        if (searchTerm) {
+          this.searchProductList = this.productsList.filter(
+            (ele) =>
+              ele.product_name
+                .toLocaleLowerCase()
+                .includes(searchTerm.toLowerCase()) ||
+              ele.hsn_code
+                .toLocaleLowerCase()
+                .includes(searchTerm.toLowerCase()),
+          );
+          return this.searchProductList;
+        }
+        this.searchProductList = this.productsList;
+        return this.searchProductList;
+      }),
+    )
+    .subscribe();
   constructor(
     private adminProductService: AdminProductService,
     private router: Router,
@@ -40,6 +63,7 @@ export class AllProductsListComponent implements OnInit, OnDestroy {
     return this.adminProductService.getAllProductsList().pipe(
       tap((productList) => {
         this.productsList = productList;
+        this.searchProductList = this.productsList;
       }),
     );
   }
--- src/app/admin/invoice/invoice-list/invoice-list.component.ts
+++ src/app/admin/invoice/invoice-list/invoice-list.component.ts
@@ -1,29 +1,81 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { InvoiceService, TGetInvoiceLists } from '../invoice.service';
-import { EMPTY, switchMap, tap } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  EMPTY,
+  Subscription,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { DatePipe, NgClass } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice-list',
-  imports: [NgClass, DatePipe],
+  imports: [NgClass, DatePipe, ReactiveFormsModule],
   templateUrl: './invoice-list.component.html',
   styleUrl: './invoice-list.component.css',
 })
-export class InvoiceListComponent implements OnInit {
+export class InvoiceListComponent implements OnInit, OnDestroy {
   private invoiceService = inject(InvoiceService);
   private router = inject(Router);
 
+  searchControl = new FormControl();
+  filterControl = new FormControl();
+
+  invoiceSubs?: Subscription;
+  searchChangeSubs = this.searchControl.valueChanges
+    .pipe(
+      debounceTime(500),
+      tap((searchTerm) => {
+        this.filterControl.setValue('all', { emitEvent: false });
+        if (searchTerm) {
+          this.searchFilterList = this.invoiceDataList.filter(
+            (ele) =>
+              ele.customer_name
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase()) ||
+              ele.invoice_number
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase()),
+          );
+          return this.searchFilterList;
+        }
+        this.searchFilterList = this.invoiceDataList;
+        return this.searchFilterList;
+      }),
+    )
+    .subscribe();
+
+  filterControlSubs = this.filterControl.valueChanges
+    .pipe(
+      distinctUntilChanged(),
+      tap((filterTerm) => {
+        if (filterTerm && filterTerm !== 'all') {
+          this.searchFilterList = this.invoiceDataList.filter((ele) =>
+            ele.invoice_status.toLowerCase().includes(filterTerm.toLowerCase()),
+          );
+          return this.searchFilterList;
+        }
+        this.searchControl.setValue('', { emitEvent: false });
+        this.searchFilterList = this.invoiceDataList;
+        return this.searchFilterList;
+      }),
+    )
+    .subscribe();
+
+  searchFilterList: TGetInvoiceLists['response']['data'] = [];
   invoiceDataList: TGetInvoiceLists['response']['data'] = [];
   ngOnInit(): void {
     this.fetchInvoiceList().subscribe();
   }
   onViewDetails(id: number) {
     this.router.navigate(['/admin/invoice/invoice-details/', id]);
-    console.log('ID');
   }
   onCancelInvoice(id: number) {
-    this.invoiceService
+    this.invoiceSubs = this.invoiceService
       .onCancelInvoice(id)
       .pipe(
         tap((res) => console.log(res)),
@@ -37,12 +89,21 @@ export class InvoiceListComponent implements OnInit {
       .subscribe();
   }
   private fetchInvoiceList() {
-    return this.invoiceService
-      .fetchInvoiceLists()
-      .pipe(tap((res) => (this.invoiceDataList = res)));
+    return this.invoiceService.fetchInvoiceLists().pipe(
+      tap((res) => {
+        this.invoiceDataList = res;
+        this.searchFilterList = this.invoiceDataList;
+      }),
+    );
   }
 
   onRoute(path: 'create-invoice' | 'invoice-lists') {
     this.router.navigate(['/admin/invoice/', path]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.filterControlSubs) this.filterControlSubs.unsubscribe();
+    if (this.searchChangeSubs) this.searchChangeSubs.unsubscribe();
+    if (this.invoiceSubs) this.invoiceSubs.unsubscribe();
   }
 }
