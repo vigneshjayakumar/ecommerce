a5a2b868c0040292e80029ff1e838702db607a90
vigneshjayakumar <iamvicky00@gmail.com>
2026-04-16 00:46:19 +0530

    Nav Header style 2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,22 @@
-import { Component, inject, OnDestroy } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { tap } from 'rxjs/internal/operators/tap';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { AuthService } from './auth/auth.service';
 import { LoaderComponent } from './common/loader/loader.component/loader.component';
-import { Subscription } from 'rxjs';
+import { BmHeaderComponent } from './ui/shared/components/bm-header/bm-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, LoaderComponent],
+  imports: [RouterOutlet, LoaderComponent, BmHeaderComponent],
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
   private authService = inject(AuthService);
-  private router = inject(Router);
-  private subs?: Subscription;
-
-  isMenuOpen = false;
-
-  isUserLoggedIn = false;
-  isUserSubs = this.authService.accessTokenObs
-    .pipe(tap((user) => (this.isUserLoggedIn = !!user)))
-    .subscribe();
 
   constructor() {
     this.authService.refreshAccessToken().subscribe();
   }
 
-  onUserLogin() {
-    this.isMenuOpen = false;
-    this.router.navigate(['/login'], { queryParams: { authPage: 'login' } });
-  }
-
-  onUserLogout() {
-    this.subs = this.authService
-      .logOut()
-      .subscribe(() => (this.isUserLoggedIn = !!this.authService.Access_token));
-  }
-  onMenuOpen() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-
-  onRouteTo(path: string) {
-    this.isMenuOpen = false;
-    this.router.navigate([path]);
-  }
-  ngOnDestroy(): void {
-    if (this.subs) this.subs.unsubscribe();
-    if (this.isUserSubs) this.isUserSubs.unsubscribe();
-  }
 }
---
+++ src/app/ui/shared/components/bm-header/bm-header.component.ts
@@ -0,0 +1,48 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/internal/operators/tap';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-bm-header',
+  imports: [],
+  templateUrl: './bm-header.component.html',
+  styleUrl: './bm-header.component.css'
+})
+export class BmHeaderComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private subs?: Subscription;
+
+  isMenuOpen = false;
+
+  isUserLoggedIn = false;
+  isUserSubs = this.authService.accessTokenObs
+    .pipe(tap((user) => (this.isUserLoggedIn = !!user)))
+    .subscribe();
+
+
+  onUserLogin() {
+    this.isMenuOpen = false;
+    this.router.navigate(['/login'], { queryParams: { authPage: 'login' } });
+  }
+
+  onUserLogout() {
+    this.subs = this.authService
+      .logOut()
+      .subscribe(() => (this.isUserLoggedIn = !!this.authService.Access_token));
+  }
+  onMenuOpen() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  onRouteTo(path: string) {
+    this.isMenuOpen = false;
+    this.router.navigate([path]);
+  }
+  ngOnDestroy(): void {
+    if (this.subs) this.subs.unsubscribe();
+    if (this.isUserSubs) this.isUserSubs.unsubscribe();
+  }
+}
