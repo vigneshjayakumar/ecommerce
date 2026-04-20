15c9bf399111670842226102d0cce205a880e38e
vigneshjayakumar <iamvicky00@gmail.com>
2026-04-21 00:20:18 +0530

    App header fix and Merchant details rendering

--- src/app/ui/shared/components/bm-header/bm-header.component.ts
+++ src/app/ui/shared/components/bm-header/bm-header.component.ts
@@ -1,7 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { EMPTY, switchMap } from 'rxjs';
 import { tap } from 'rxjs/internal/operators/tap';
 import { Subscription } from 'rxjs/internal/Subscription';
+import { InvoiceService } from 'src/app/admin/invoice/invoice.service';
 
 import { AuthService } from 'src/app/auth/auth.service';
 
@@ -14,12 +16,35 @@ import { AuthService } from 'src/app/auth/auth.service';
 export class BmHeaderComponent {
   private authService = inject(AuthService);
   private router = inject(Router);
+  private invoiceService = inject(InvoiceService);
   private subs?: Subscription;
 
   isUserLoggedIn = false;
   isUserSubs = this.authService.accessTokenObs
-    .pipe(tap((user) => (this.isUserLoggedIn = !!user)))
+    .pipe(tap((user) => (this.isUserLoggedIn = !!user))).pipe(switchMap((user) => {
+      if (!!user) {
+        return this.fetchMerchantDetails()
+      }
+      return EMPTY
+    }))
     .subscribe();
+
+  merchantDetails = {
+    name: '',
+    address: '',
+    state: '',
+    pincode: ''
+  }
+  private fetchMerchantDetails() {
+    return this.invoiceService.fetchMerchentDetails().pipe(tap(res => {
+      this.merchantDetails = {
+        name: res.tenant_name,
+        address: res.address,
+        state: res.state,
+        pincode: res.pincode.toString()
+      }
+    }))
+  }
 
   onUserLogin() {
     this.router.navigate(['/login'], { queryParams: { authPage: 'login' } });
@@ -32,7 +57,7 @@ export class BmHeaderComponent {
   }
 
   onRouteTo(path: string) {
-    console.log('PATH',path)
+    console.log('PATH', path)
     this.router.navigate([path]);
   }
   ngOnDestroy(): void {
