f0e6b737e47000dd397903f96ecf7fc0eae48b52
vigneshjayakumar <iamvicky00@gmail.com>
2026-05-04 23:18:50 +0530

    Clean up

--- src/app/branch-wise/branch.routes.ts
+++ src/app/branch-wise/branch.routes.ts
@@ -4,7 +4,6 @@ import { CreateBranchComponent } from "./create-branch/create-branch.component";
 import { AllocateProductBranchwiseComponent } from "./allocate-product-branchwise/allocate-product-branchwise.component";
 import { BranchWiseComponent } from "./branch-wise.component";
 import { BranchListComponent } from "./branch-list/branch-list.component";
-import { ViewEditBranchComponent } from "./view-edit-branch/view-edit-branch.component";
 
 export const branchwiseRoutes: Routes = [
     {
@@ -14,7 +13,6 @@ export const branchwiseRoutes: Routes = [
                 'path': 'create-branch', component: CreateBranchComponent
             },
             { path: 'list', component: BranchListComponent },
-            { path: 'view-edit-branch', component: ViewEditBranchComponent },
             {
                 path: 'allocate-product', component: AllocateProductBranchwiseComponent
             },
